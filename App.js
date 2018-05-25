a94b6c80c5fbd3bb2db6352e96d2d9f16852a782
Samantha Geitz <samanthamichele7@gmail.com>
2018-05-25 10:47:05 -0500

    Initial commit oops should have done this already

---
+++ App.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      text: ''
+    }
+  }
+
+  createCategory() {
+    fetch('http://pickr-api.test/api/categories', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: this.state.text,
+      }),
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson);
+      });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>Add a new category</Text>
+
+        <TextInput
+          style={styles.textInput}
+          onChangeText={(text) => this.setState({text})}
+          placeholder="Category Name"
+          value={this.state.text}
+        />
+
+        <Button
+          onPress={() => this.createCategory()}
+          title="Add"
+          accessibilityLabel="Add a new category"
+        />
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    fontSize: 20
+  },
+  textInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    width: 250
+  }
+});
